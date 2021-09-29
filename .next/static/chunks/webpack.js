7a6f3151502365e56b5d12e0a678e9549b9a0257
niyera <nilaniyera@gmail.com>
2021-09-29 12:00:41 +0200

    removing unecessary code

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "edb368387ea2e5461dc3"; }
+/******/ 		__webpack_require__.h = function() { return "4f3a7577d997cca28d4d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
